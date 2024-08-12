697dec7efbd4e9cd3bb9145de4892b95a27291fa
henohenon <takeru.hawk.son@gmail.com>
2024-08-12 13:08:22 +0900

    pooling and remove http-server

--- ar/henohenos/vite.config.js
+++ ar/henohenos/vite.config.js
@@ -4,6 +4,9 @@ export default defineConfig({
     root: './src',
     server: {
         host: '0.0.0.0',
-        port: 8000
+        port: 8000,
+        watch: {
+            usePolling: true,  // ポーリングを有効にする
+        },
     },
 });
