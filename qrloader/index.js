627d5a3fa084d6667a99e0d3d62a81027b94dbda
henohenon <takeru.hawk.son@gmail.com>
2024-10-08 09:25:49 +0900

    format scripts

--- qrloader/index.js
+++ qrloader/index.js
@@ -9,13 +9,8 @@ navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
   video.play();
 });
 
-//let threeScene;
-let mesh;
-let renderer;
-let scene;
-let camera;
-let vertices;
-let geometry;
+let myThree;
+let arConverter;
 
 video.addEventListener("loadedmetadata", () => {
   cameraCanvas.width = video.videoWidth;
@@ -23,24 +18,11 @@ video.addEventListener("loadedmetadata", () => {
   arCanvas.width = video.videoWidth;
   arCanvas.height = video.videoHeight;
 
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(
-    75,
-    cameraCanvas.width / cameraCanvas.height,
-    0.1,
-    1000
+  myThree = new MyThree(arCanvas);
+  arConverter = new ARCoordinateTransformer(
+    cameraCanvas.width,
+    cameraCanvas.height
   );
-  renderer = new THREE.WebGLRenderer({
-    canvas: arCanvas,
-  });
-  renderer.setSize(cameraCanvas.width, cameraCanvas.height);
-  geometry = new THREE.BoxGeometry(2, 2, 2); // 2x2x2の立方体
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
-  camera.position.z = 5;
-
-  //console.log(geometry.attributes.position.array);
 
   qrScan();
 });
@@ -56,19 +38,6 @@ function qrScan() {
     cameraCanvas.height
   );
 
-  // カメラの中心に十字を描画
-  context.strokeStyle = "white";
-  context.lineWidth = 3;
-  context.beginPath();
-  context.moveTo(cameraCanvas.width / 2, 0);
-  context.lineTo(cameraCanvas.width / 2, cameraCanvas.height);
-  context.moveTo(0, cameraCanvas.height / 2);
-  context.lineTo(cameraCanvas.width, cameraCanvas.height / 2);
-  context.lineWidth = 2;
-  context.moveTo(cameraCanvas.width / 4, 0);
-  context.lineTo(cameraCanvas.width / 4, cameraCanvas.height);
-  context.stroke();
-
   const qrCode = jsQR(imageData.data, imageData.width, imageData.height);
   if (qrCode) {
     const {
@@ -81,17 +50,13 @@ function qrScan() {
     //console.log("QRコード内容:", qrCode);
 
     // qrの縁に線を描画する
-    context.strokeStyle = "yellow";
+    drawQRBorder(
+      topLeftCorner,
+      topRightCorner,
+      bottomRightCorner,
+      bottomLeftCorner
+    );
 
-    context.beginPath();
-    context.moveTo(topLeftCorner.x, topLeftCorner.y);
-    context.lineTo(topRightCorner.x, topRightCorner.y);
-    context.lineTo(bottomRightCorner.x, bottomRightCorner.y);
-    context.lineTo(bottomLeftCorner.x, bottomLeftCorner.y);
-    context.closePath();
-    context.stroke();
-    const chWidth = cameraCanvas.width / 2;
-    const chHeight = cameraCanvas.height / 2;
     // QRコードの座標
     const twoDPoints = [
       topLeftCorner.x,
@@ -103,69 +68,144 @@ function qrScan() {
       bottomRightCorner.x,
       bottomRightCorner.y,
     ];
+
+    console.log(arConverter);
+
+    const { rvec, tvec } = arConverter.qrPointsTo3DPoints(twoDPoints);
+
+    // 回転ベクトルと平行移動ベクトルを表示
+    console.log("Rotation Vector:", rvec.data64F);
+    console.log("Translation Vector:", tvec.data64F);
+
+    // 3D座標を更新
+    myThree.update(tvec, rvec);
+  }
+
+  requestAnimationFrame(qrScan);
+}
+
+function drawQRBorder(
+  topLeftCorner,
+  topRightCorner,
+  bottomRightCorner,
+  bottomLeftCorner
+) {
+  context.strokeStyle = "yellow";
+
+  context.beginPath();
+  context.moveTo(topLeftCorner.x, topLeftCorner.y);
+  context.lineTo(topRightCorner.x, topRightCorner.y);
+  context.lineTo(bottomRightCorner.x, bottomRightCorner.y);
+  context.lineTo(bottomLeftCorner.x, bottomLeftCorner.y);
+  context.closePath();
+  context.stroke();
+}
+
+class ARCoordinateTransformer {
+  threeDPoints;
+  fx;
+  fy;
+  cx;
+  cy;
+  cameraMatrix;
+  distCoeffs;
+
+  constructor(width, height) {
     // 3d座標
-    const threeDPoints = [1, 1, 0, -1, 1, 0, 1, -1, 0, -1, -1, 0];
+    this.threeDPoints = [1, 1, 0, -1, 1, 0, 1, -1, 0, -1, -1, 0];
+    this.fx = 300; // 焦点距離の例
+    this.fy = 300; // 焦点距離の例
+    this.cx = width / 2; // カメラ中心（例）
+    this.cy = height / 2; // カメラ中心（例）
 
-    const fx = 300; // 焦点距離の例
-    const fy = 300; // 焦点距離の例
-    const cx = chWidth; // カメラ中心（例）
-    const cy = chHeight; // カメラ中心（例）
-
-    const cameraMatrix = cv.matFromArray(3, 3, cv.CV_64F, [
-      fx,
+    this.cameraMatrix = cv.matFromArray(3, 3, cv.CV_64F, [
+      this.fx,
       0,
-      cx,
+      this.cx,
       0,
-      fy,
-      cy,
+      this.fy,
+      this.cy,
       0,
       0,
       1,
     ]);
 
     // 歪み係数（ゼロに設定）
-    const distCoeffs = cv.Mat.zeros(4, 1, cv.CV_64F);
+    this.distCoeffs = cv.Mat.zeros(4, 1, cv.CV_64F);
+  }
 
+  qrPointsTo3DPoints(qrPoints) {
     // 変数の初期化
     const rvec = new cv.Mat();
     const tvec = new cv.Mat();
 
     // SolvePnPでカメラの外部パラメータを推定
-    const objectPointsMat = cv.matFromArray(4, 3, cv.CV_32F, threeDPoints);
+    const objectPointsMat = cv.matFromArray(4, 3, cv.CV_32F, this.threeDPoints);
 
-    const imagePointsMat = cv.matFromArray(4, 2, cv.CV_32F, twoDPoints);
+    const imagePointsMat = cv.matFromArray(4, 2, cv.CV_32F, qrPoints);
 
     cv.solvePnP(
       objectPointsMat,
       imagePointsMat,
-      cameraMatrix,
-      distCoeffs,
+      this.cameraMatrix,
+      this.distCoeffs,
       rvec,
       tvec
     );
 
-    // 回転ベクトルと平行移動ベクトルを表示
-    console.log("Rotation Vector:", rvec.data64F);
-    console.log("Translation Vector:", tvec.data64F);
+    return { rvec, tvec };
+  }
+}
+class MyThree {
+  scene;
+  camera;
+  renderer;
+  mesh;
+
+  constructor(canvas) {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      canvas.width / canvas.height,
+      0.1,
+      1000
+    );
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+    });
+    this.renderer.setSize(canvas.width, canvas.height);
+    const geometry = new THREE.BoxGeometry(2, 2, 2); // 2x2x2の立方体
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(this.mesh);
+    this.camera.position.z = 5;
+  }
+
+  update(tvec, rvec) {
+    // Three.jsのカメラに位置ベクトルを適用
+    const tvecData = tvec.data64F;
+    this.camera.position.set(-tvecData[0], tvecData[1], tvecData[2]);
 
     // 回転ベクトルを回転行列に変換（Rodrigues変換）
     const rotationMatrix = new cv.Mat();
     cv.Rodrigues(rvec, rotationMatrix); // rvecを回転行列に変換
 
+    const rotationData = rotationMatrix.data64F;
+
     // Three.jsのカメラに回転行列を適用
     const threeRotationMatrix = new THREE.Matrix4();
     threeRotationMatrix.set(
-      rotationMatrix.data64F[0],
-      -rotationMatrix.data64F[1],
-      -rotationMatrix.data64F[2],
+      rotationData[0],
+      -rotationData[1],
+      -rotationData[2],
       0,
-      -rotationMatrix.data64F[3],
-      rotationMatrix.data64F[4],
-      rotationMatrix.data64F[5],
+      -rotationData[3],
+      rotationData[4],
+      rotationData[5],
       0,
-      -rotationMatrix.data64F[6],
-      rotationMatrix.data64F[7],
-      rotationMatrix.data64F[8],
+      -rotationData[6],
+      rotationData[7],
+      rotationData[8],
       0,
       0,
       0,
@@ -173,29 +213,15 @@ function qrScan() {
       1
     );
 
-    /*
-          rotationMatrix.makeRotationFromEuler(
-        new THREE.Euler(
-          rotationVector.x,
-          rotationVector.y,
-          rotationVector.z,
-          "XYZ"
-        )
-      )
-
-    */
-    camera.position.set(-tvec.data64F[0], tvec.data64F[1], tvec.data64F[2]);
-
+    // カメラの姿勢を設定
     const quaternion = new THREE.Quaternion();
     quaternion.setFromRotationMatrix(threeRotationMatrix);
-    mesh.setRotationFromQuaternion(quaternion);
+    this.mesh.setRotationFromQuaternion(quaternion);
 
     // カメラの更新
-    mesh.updateMatrixWorld();
-    camera.updateMatrixWorld();
+    this.mesh.updateMatrixWorld();
+    this.camera.updateMatrixWorld();
 
-    renderer.render(scene, camera);
+    this.renderer.render(this.scene, this.camera);
   }
-
-  requestAnimationFrame(qrScan);
 }
